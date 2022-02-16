b727cf1d5e79bc33d99340ef710dcc40d6974250
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-02-16 04:28:14 +0000

    Update PlayerController.cs
    Minor values adjustment
    updated the input manager in
    Edit > Project Settings >Input Manager
    
    This handles your ButtonDown values.
    
    Made it so that it could jump with arrow keys and also with wasd keys instead of standard spacebar.
    
    Added comments about the jump too

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : PhysicsObject
 {
     public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
+    public float jumpTakeOffSpeed = 8;
 
     private SpriteRenderer spriteRenderer;
     //private Animator animator;
@@ -21,6 +21,9 @@ public class PlayerController : PhysicsObject
     {
         Vector2 move = Vector2.zero;
 
+
+        //lets you do dynamic jump i.e if you hold up button then you jump higher
+        //else if you press and release its just a jump
         move.x = Input.GetAxis("Horizontal");
 
         if (Input.GetButtonDown("Jump") && grounded)
