f75757451b20ace3914ab0c23fd75f8401054bbc
Y0URFISHY <Dex9711@live.co.uk>
2022-03-31 22:24:23 +0100

    State Switch Colliders Fixed
    Music Manager Game Object added - Switches states
    Player Controller - Collider trigger for state change - line 569-585
    Collider alteration - Music_ChangeBC = Behemoth Combat track

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    //Wwise Changes state of Play_Music_Tracks Event
+    static public void SetBehemothCombat() 
+    {
+        AkSoundEngine.SetState("Current_Location", "Boss_Room60");
+    }
+    static public void SetBehemothIntro()
+    {
+        AkSoundEngine.SetState("Current_Location", "Boss_Intro60");
+    }
+
+    static public void SetNomadMain()
+    {
+        AkSoundEngine.SetState("Current_Location", "Exploration77");
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -566,6 +566,23 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Wwise Music_Change Colliders - Behemoth Combat
+        if (other.gameObject.name == "Music_ChangeBC") 
+        {
+            MusicManager.SetBehemothCombat(); //Change Event State to Boss_Room60
+        } 
+        else if (other.gameObject.name == "Music_ChangeBI")
+        {
+            MusicManager.SetBehemothIntro(); // Change Event State to Boss_Intro60
+        }
+        else if (other.gameObject.name == "Music_ChangeNM")
+        {
+            MusicManager.SetNomadMain(); // Change Event State to Exploration77
+        }
+    }
+
     #region Input Methods
     public void OnMovement(InputAction.CallbackContext context)
     {
