ba6d385407d14a0e4100bcac0289f9c162cf4a1e
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-04-21 00:28:57 +0100

    Pause menu stuff

---
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public string mainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResumeGame()
+    {
+
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+}
