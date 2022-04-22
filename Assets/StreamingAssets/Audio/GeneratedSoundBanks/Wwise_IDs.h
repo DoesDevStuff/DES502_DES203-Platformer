f2c4903a6bbd79e7c3e64caf52c34a22e38d6064
MopeydropTTW <jojorob1512@googlemail.com>
2022-04-22 15:58:12 +0100

    SFX
    Sound effects, more to be added, moving to the main branch

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,15 +13,39 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BAT_AMBIENT = 1829904715U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID EERIE_SOUND = 101911589U;
+        static const AkUniqueID FG_1 = 2227562272U;
+        static const AkUniqueID FLARE_EXPLOSION = 1207279315U;
+        static const AkUniqueID FLARESHOT = 3944494847U;
+        static const AkUniqueID HIT_SOUND = 3867410532U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID MENU_SOUND = 1645260788U;
+        static const AkUniqueID MONSTER_HOWL = 2183591810U;
+        static const AkUniqueID SOMETHING = 4191063435U;
+        static const AkUniqueID TETHERED_SOUND = 785538926U;
         static const AkUniqueID WALK = 2108779966U;
+        static const AkUniqueID WIND_SOUND = 3392291667U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BAT_AMBIENT = 1829904715U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID EERIE_SOUND = 101911589U;
+        static const AkUniqueID FG_1 = 2227562272U;
+        static const AkUniqueID FLARE_EXPLOSION = 1207279315U;
         static const AkUniqueID FLARESHOT = 3944494847U;
+        static const AkUniqueID HIT_SOUND = 3867410532U;
         static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID MENU_SOUND = 1645260788U;
+        static const AkUniqueID MONSTER_HOWL = 2183591810U;
+        static const AkUniqueID SOMETHING = 4191063435U;
+        static const AkUniqueID TETHERED_SOUND = 785538926U;
         static const AkUniqueID WALK = 2108779966U;
+        static const AkUniqueID WIND_SOUND = 3392291667U;
     } // namespace BANKS
 
     namespace BUSSES
