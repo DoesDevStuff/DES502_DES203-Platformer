2fcbdcf0722e54897ec87e90631a5a8cb34aeb3b
CharlieTheIndieDev <74312830+CharlieTheIndieDev@users.noreply.github.com>
2022-04-25 23:07:39 +0100

    Pause Menu added
    Pause meu script was created and tested all things work
    
    @Y0URFISHY  Can you check the audio states and see if they are working with the menu as you would like them to once this is merged?
    Think the audio has some overlap

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -18,16 +18,32 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f; // freeze the game scene
+
+            }
+        }
     }
 
     public void ResumeGame()
     {
-
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     public void ReturnToMain()
     {
+        Time.timeScale = 1f; // set timescale back to 1 or normal
         SceneManager.LoadScene(mainMenuScene);
     }
 }
