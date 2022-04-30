d160d58c0761bb2532a2464ed1379005cf2e142a
MopeydropTTW <jojorob1512@googlemail.com>
2022-04-30 11:21:52 +0100

    Tweaks
    Just tweaked some other audio, still cant get this walkk to work, the turt I use is saying to use Time.time but its not doing anything

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -187,7 +187,7 @@ public class PlayerController : MonoBehaviour
                             WalkIsPlaying = false;
                         }
                     }
-                    
+
                 }
             }
 
